2fa567523e443465fe2d1d5eee05361a23a8ca7e
MSAZ89 <michaelsimonaz@gmail.com>
2022-10-17 12:41:29 -0700

    card data testing

--- src/components/cardgrid.js
+++ src/components/cardgrid.js
@@ -1,11 +1,17 @@
 import MyCard from "./mycard"
 import { SimpleGrid } from '@chakra-ui/react'
 
-export default function CardGrid(){
+export default function CardGrid(props){
+    
+    const cardList = props.data.map((item) => {
+        return (
+          <MyCard key={item.title} title={item.title} desc={item.desc}/>
+        )
+    });
+
     return (
-        <SimpleGrid minChildWidth='200px' columns={2} spacing={5}>
-            <MyCard title="title" desc="desciption text." />
-            <MyCard title="title 2" desc="desciption text 2." />
+        <SimpleGrid maxW="full" minChildWidth='200px' columns={2} spacing={5}>
+            {cardList}
         </SimpleGrid>
     )
 }
