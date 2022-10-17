d2a238af3eeb33d4b7f8d54d10925368d7b6a709
MSAZ89 <michaelsimonaz@gmail.com>
2022-10-17 12:41:54 -0700

    card data testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
-import { Container, Box, } from '@chakra-ui/react';
+import { Container, Box, Button } from '@chakra-ui/react';
 import Header from './components/header';
 import CardGrid from './components/cardgrid';
+import { useState } from 'react';
 
 function App() {
+
+let newCardId = 1;
+
+const [data, setData] = useState([
+  {title: "card 0", desc: "card 0 description"},
+])
+
+function AppendData(){
+  setData((array) => [...array, {title: "card " + newCardId, desc: "card " + newCardId + " description"}]);
+  newCardId++;
+  console.log(newCardId);
+}
+
   return (
     <Container maxW='7xl' className='App'>
       <Box boxShadow='lg' p='6' rounded='md' bg='gray.100' mt="4">
         <Header h1='Page Title' desc="Page Description"/>
-        <CardGrid/>
+        <Button onClick={() => AppendData()}>Append</Button>
+        <CardGrid data={data}/>
       </Box>
     </Container>
   );
--- src/components/mycard.js
+++ src/components/mycard.js
@@ -1,9 +1,9 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Heading } from "@chakra-ui/react"
 
 export default function MyCard(props){
     return (
         <Box boxShadow='lg' p='4' rounded='md' bg='white'>
-            <h2>{props.title}</h2>
+            <Heading as='h2' size='md'>{props.title}</Heading>
             <p>{props.desc}</p>
         </Box>
     )
