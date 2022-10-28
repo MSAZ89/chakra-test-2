8440aa848bcb45034b6e62d9ed38c622b9bc9261
MSAZ89 <michaelsimonaz@gmail.com>
2022-10-28 11:48:31 -0700

    fixed removing item from data array

--- src/App.js
+++ src/App.js
@@ -8,30 +8,28 @@ let newCardId = 0;
 
 function App() {
 
-const [data, setData] = useState([
+  const [data, setData] = useState([])
 
-])
+  function AppendData(){
+    newCardId++;
+    setData((array) => [...array, {title: "card " + newCardId, desc: "card " + newCardId + " description"}]);
+    //console.log(newCardId);
+  }
 
-function AppendData(){
-  newCardId++;
-  setData((array) => [...array, {title: "card " + newCardId, desc: "card " + newCardId + " description"}]);
-  console.log(newCardId);
-}
-
-function PopData(){
-  newCardId--;
-  let newdata = data;
-  newdata.pop();
-  console.log(newdata);
-  setData(newdata);
-}
+  function FilterAndUpdateData(){
+    if (newCardId >= 1) {
+      newCardId--;
+      setData((data) => data.filter((_, lastItem) => lastItem !== data.length - 1));
+      //console.log(data);
+    }
+  }
 
   return (
     <Container maxW='7xl' className='App'>
       <Box boxShadow='lg' p='6' rounded='md' bg='gray.100' mt="4">
         <Header h1='Page Title' desc="Page Description"/>
         <Button variant='outline' size='sm' colorScheme='blue' onClick={() => AppendData()}>Append</Button>
-        <Button variant='outline' size='sm' colorScheme='blue' onClick={() => PopData()}>Pop</Button>
+        <Button variant='outline' size='sm' colorScheme='blue' onClick={() => FilterAndUpdateData()}>Remove</Button>
         <br/><br/>
         <CardGrid data={data}/>
       </Box>
